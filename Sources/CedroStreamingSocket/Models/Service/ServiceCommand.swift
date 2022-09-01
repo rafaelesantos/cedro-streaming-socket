77bde3ff474e3646f1494834762e4a95c4b5c870
Rafael Escaleira <rafael.escaleira@deliverit.com.br>
2022-09-01 18:17:00 -0400

    Cast string to data value to input on socket

--- Sources/CedroStreamingSocket/Models/Service/ServiceCommand.swift
+++ Sources/CedroStreamingSocket/Models/Service/ServiceCommand.swift
@@ -6,21 +6,30 @@ public enum ServiceCommand {
     case player(market: Market)
     case aggregatedBook(asset: String)
     
-    var subscribe: String {
+    var subscribe: Data? {
         switch self {
-        case .login(let authentication): return "ftcedro\n\(authentication.username)\n\(authentication.password)\n"
-        case .bookQuote(let asset): return "bqt \(asset.lowercased())\n"
-        case .player(let market): return "gpn \(market.rawValue.lowercased())\n"
-        case .aggregatedBook(let asset): return "sab \(asset.lowercased())\n"
+        case .login(let authentication): return "ftcedro\n\(authentication.username)\n\(authentication.password)\n".dataValue
+        case .bookQuote(let asset): return "bqt \(asset.lowercased())\n".dataValue
+        case .player(let market): return "gpn \(market.rawValue.lowercased())\n".dataValue
+        case .aggregatedBook(let asset): return "sab \(asset.lowercased())\n".dataValue
         }
     }
     
-    var unsubscribe: String {
+    var unsubscribe: Data? {
         switch self {
-        case .login(_): return "quit\n"
-        case .bookQuote(let asset): return "ubq \(asset.lowercased())\n"
-        case .player(_): return ""
-        case .aggregatedBook(let asset): return "uab \(asset.lowercased())\n"
+        case .login(_): return "quit\n".dataValue
+        case .bookQuote(let asset): return "ubq \(asset.lowercased())\n".dataValue
+        case .player(_): return "".dataValue
+        case .aggregatedBook(let asset): return "uab \(asset.lowercased())\n".dataValue
+        }
+    }
+    
+    var tag: Int {
+        switch self {
+        case .login(_): return 1
+        case .bookQuote(_): return 2
+        case .player(_): return 3
+        case .aggregatedBook(_): return 4
         }
     }
 }
